474ced593f152dfb22446cacfa7efccbddaa7b82
Gordon Bailey <grodtron@users.noreply.github.com>
2024-02-05 00:04:39 +0100

    Update event_handler_factory.py

--- cultivator/event_handler_factory.py
+++ cultivator/event_handler_factory.py
@@ -2,11 +2,11 @@ from cultivator.models.event import Event
 from cultivator.event_handlers import EventHandler, PushEventHandler, IssueEventHandler
 from cultivator.api_clients import GitHubAPIClient
 from cultivator.models.platform_enum import Platform
-from typing import Optional
+from typing import Optional, Dict, Any
 
 class EventHandlerFactory:
     @staticmethod
-    def get_handler(event: Event, api_key: str, config: Optional[dict] = None) -> EventHandler:
+    def get_handler(event: Event, api_key: str, config: Optional[Dict[str, Any]] = None) -> EventHandler:
         platform_client_mapping = {
             Platform.GITHUB: GitHubAPIClient
         }
