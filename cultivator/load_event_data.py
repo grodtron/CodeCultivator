f4ee31e874df56a6e29b2099fcc8d3e5edb2717f
Gordon Bailey <grodtron@users.noreply.github.com>
2024-02-06 01:28:38 +0100

    Create load_event_data.py

---
+++ cultivator/load_event_data.py
@@ -0,0 +1,16 @@
+from cultivator.models.platform_enum import Platform
+from cultivator.models.event import Event
+
+
+def load_event_data() -> Event:
+    try:
+        if "GITHUB_EVENT_PATH" in os.environ:
+            platform = Platform.GITHUB
+            event_path = os.environ["GITHUB_EVENT_PATH"]
+            with open(event_path, 'r') as event_file:
+                data = json.load(event_file)
+            return Event(platform, data)
+        else:
+            raise ValueError("Platform cannot be detected.")
+    except Exception as e:
+        raise RuntimeError(f"Error loading event data: {e}")
